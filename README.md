132a36ae2e32abd86059ec0a8d417d8250af8ef6
Alex Ting <alex@appcues.com>
2017-02-08 13:49:05 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Boilerplate
+
+### Instructions for viewing:
+
+1. Cd into root directory.
+2. Run 'npm install' in terminal.
+3. Run 'npm start' in terminal.
+4. Go to localhost:3000 in browser.
---
+++ client/app/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div>
+          <h1>Hello World</h1>
+      </div>
+    );
+  };
+}
+
+export default App;
